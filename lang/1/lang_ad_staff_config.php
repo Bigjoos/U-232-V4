732313cdc3bdee373bb0ebc6791704860ff14de4
Bigjoos <Bigjoos1@gmail.com>
2014-08-02 15:17:11 +0000

    Language typo's fixed

--- lang/1/lang_ad_staff_config.php
+++ lang/1/lang_ad_staff_config.php
@@ -1,7 +1,7 @@
 <?php
 $lang = array(
-	'staffcfg_file_created' => "?php\n/**\nThis file created on ",
-	'staffcfg_mod_by' => ".\nSite Config staff mod by pdq/U-232.\n**/\n",
+	'staffcfg_file_created' => "\nThis file created on ", 
+	'staffcfg_mod_by' => ".\nSite Config staff mod by pdq/U-232.\n",
 	'staffcfg_success' => "'Success",
 	'staffcfg_updated' => "Both staff configuration files were updated",
 	'staffcfg_stdhead' => "Staff Config"
