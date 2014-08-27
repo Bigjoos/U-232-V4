78a830503c1af29f931ff1b261112bba0c4ebc0f
iseeyoucopy <ivan.catalin.aurel@gmail.com>
2014-08-27 22:23:05 +0200

    Update lang_ad_comment_check.php
    Romanian language

--- lang/3/lang_ad_comment_check.php
+++ lang/3/lang_ad_comment_check.php
@@ -1,15 +1,15 @@
 <?php
 $lang = array(
-	'comm_check_success' => "Success",
-	'comm_check_successfully' => "Comments successfully checked",
-	'comm_check_user' => "User",
+	'comm_check_success' => "Succes",
+	'comm_check_successfully' => "Comentarii verificate cu succes",
+	'comm_check_user' => "Utilizator",
 	'comm_check_text' => "Text",
-	'comm_check_date' => "Date",
-	'comm_check_action' => "Actions",
-	'comm_check_edit' => "Edit",
-	'comm_check_chk' => "Checked.",
-	'comm_check_chk1' => "Checked?",
-	'comm_check_read' => "Mark as checked",
-	'comm_check_stdhead' => "Comment checks"
+	'comm_check_date' => "Data",
+	'comm_check_action' => "Actiuni",
+	'comm_check_edit' => "Editeaza",
+	'comm_check_chk' => "Verificat.",
+	'comm_check_chk1' => "Verificat?",
+	'comm_check_read' => "Marcheaza ca verificat",
+	'comm_check_stdhead' => "Verifica comentarii"
 );
 ?>
