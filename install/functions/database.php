73eca5d3be944dfe75eaf6fb30ed064e69e8fff1
whocares <whocares@open-scene.net>
2014-09-02 00:09:02 -0400

    Update database.php

--- install/functions/database.php
+++ install/functions/database.php
@@ -8,7 +8,7 @@ function db_test()
         $out.= '<div class="readable">Connection to database was made</div>';
         if (mysql_select_db($INSTALLER09['mysql_db'])) {
             $out.= '<div class="readable">Data base exists, data can be imported</div>';
-            $out.= '<form action="index.php" method="post"><div class="info" style="text-align:center;"><input type="hidden" name="do" value="db_insert" /><input type="submit" value="Import database" /></div></form>';
+            $out.= '<form action="index.php" method="post"><div class="info" style="text-align:center;"><input type="hidden" name="do" value="db_insert" /><input type="hidden" name="xbt" value="'.$_GET['xbt'].'" /><input type="submit" value="Import database" /></div></form>';
         } else $out.= '<div class="notreadable">There was an error while selecting the database<br/>'.mysql_error().'</div><div class="info" style="text-align:center"><input type="button" value="Reload" onclick="window.location.reload()"/></div>';
     } else $out.= '<div class="notreadable">There was an error while connection to the database<br/>'.mysql_error().'</div><div class="info" style="text-align:center"><input type="button" value="Reload" onclick="window.location.reload()"/></div>';
     $out.= '</fieldset>';
@@ -20,8 +20,10 @@ function db_insert()
     global $root, $INSTALLER09;
     $out = '<fieldset><legend>Database</legend>';
     require_once ($root.'include/config.php');
-    $q = sprintf('/usr/bin/mysql -h %s -u %s -p%s %s < %sinstall/extra/install.sql', $INSTALLER09['mysql_host'], $INSTALLER09['mysql_user'], $INSTALLER09['mysql_pass'], $INSTALLER09['mysql_db'], $root); //== Linux
-    //$q = sprintf('c:\AppServ\MySQL\bin\mysql -h %s -u %s -p%s %s < %sinstall/extra/install.sql',$INSTALLER09['mysql_host'],$INSTALLER09['mysql_user'],$INSTALLER09['mysql_pass'],$INSTALLER09['mysql_db'],$root);  //== Win - remember to set your path up correctly - atm its set for appserv
+    if($_POST['xbt'] == 1) $file = "install.xbt.sql";
+    elseif($_POST['xbt'] == 0) $file = "install.php.sql";
+    $q = sprintf('/usr/bin/mysql -h %s -u %s -p%s %s < %sinstall/extra/'.$file, $INSTALLER09['mysql_host'], $INSTALLER09['mysql_user'], $INSTALLER09['mysql_pass'], $INSTALLER09['mysql_db'], $root); //== Linux
+    //$q = sprintf('c:\AppServ\MySQL\bin\mysql -h %s -u %s -p%s %s < %sinstall/extra/'.$file,$INSTALLER09['mysql_host'],$INSTALLER09['mysql_user'],$INSTALLER09['mysql_pass'],$INSTALLER09['mysql_db'],$root);  //== Win - remember to set your path up correctly - atm its set for appserv
     exec($q, $o);
     if (!count($o)) {
         $out.= '<div class="readable">Database was imported</div><div class="info" style="text-align:center"><input type="button" value="Finish" onclick="window.location.href=\'?step=3\'"/></div>';
