2940e38ed4e2ce9cb303b2fe6d72ee4e1e49d9f7
whocares <whocares@open-scene.net>
2014-10-23 19:19:38 -0400

    Update visible_update.php

--- include/cleanup/visible_update.php
+++ include/cleanup/visible_update.php
@@ -24,6 +24,9 @@ function docleanup($data)
     $deadtime_tor = TIME_NOW - $INSTALLER09['max_dead_torrent_time'];
     $What_Time = (XBT_TRACKER == true ? 'mtime' : 'last_action'); 
     sql_query("UPDATE torrents SET visible='no' WHERE visible='yes' AND $What_Time < $deadtime_tor");
+    if (XBT_TRACKER == true) {
+     sql_query("UPDATE torrents SET visible='yes' WHERE visible='no' AND seeders > 0");
+    }
     if ($queries > 0) write_log("Torrent Visible clean-------------------- Torrent Visible cleanup Complete using $queries queries --------------------");
     if (false !== mysqli_affected_rows($GLOBALS["___mysqli_ston"])) {
         $data['clean_desc'] = mysqli_affected_rows($GLOBALS["___mysqli_ston"]) . " items deleted/updated";
