a2c8ffc1e526ec1be23fc47a234c40f77118c36f
StNr <flushedaway@live.co.uk>
2015-11-07 17:36:53 +0000

    Update completed.php
    Added default to catch anything missed.  I see this has been changed from the announce.php one.

--- blocks/userdetails/completed.php
+++ blocks/userdetails/completed.php
@@ -50,6 +50,10 @@ if ($INSTALLER09['hnr_online'] == 1 && $user['paranoia'] < 2 || $CURUSER['id'] =
                 $days_14 = $INSTALLER09['_14day_third'] * 3600; //== 12 hours
                 $days_over_14 = $INSTALLER09['_14day_over_third'] * 3600; //== 12 hours
                 break;
+            default:
+            $days_3 = 0; //== 12 hours
+            $days_14 = 0; //== 12 hours
+            $days_over_14 = 0; //== 12 hours
             }
             //=== times per torrent based on age
             $foo = $a['downloaded'] > 0 ? $a['uploaded'] / $a['downloaded'] : 0;
