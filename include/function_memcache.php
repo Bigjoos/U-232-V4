a158ccc107a13c1b79e9aec05954e8d19b3c5d43
Bigjoos <Bigjoos1@gmail.com>
2016-10-15 20:05:58 +0000

    Mysqli update;

--- include/function_memcache.php
+++ include/function_memcache.php
@@ -17,17 +17,6 @@
  \_/ \_/ \_/ \_/ \_/   \_/ \_/ \_/ \_/ \_/ \_/   \_/ \_/ \_/ \_/
  */
 // removetorrentfromhash djGrrr <3
-function remove_torrent($infohash)
-{
-    global $mc1;
-    if (strlen($infohash) != 40 || !bin2hex($infohash)) return false;
-    $key = 'torrent::hash:::' . md5($infohash);
-    $torrent = $mc1->get_value($key);
-    if ($torrent === false) return false;
-    $mc1->delete_value($key);
-    if (is_array($torrent)) remove_torrent_peers($torrent['id']);
-    return true;
-}
 function remove_torrent_peers($id)
 {
     global $mc1;
@@ -40,5 +29,16 @@ function remove_torrent_peers($id)
     $delete+= $mc1->delete_value($leech_key);
     $delete+= $mc1->delete_value($comp_key);
     return (bool)$delete;
+}
+function remove_torrent($infohash)
+{
+    global $mc1;
+    if (strlen($infohash) != 20 || !bin2hex($infohash)) return false;
+    $key = 'torrent::hash:::' . md5($infohash);
+    $torrent = $mc1->get_value($key);
+    if ($torrent === false) return false;
+    $mc1->delete_value($key);
+    if (is_array($torrent)) remove_torrent_peers($torrent['id']);
+    return true;
 }
 ?>
